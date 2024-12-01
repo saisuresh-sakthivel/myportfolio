ee35c8fc63642dab07419267041543bf52599479
= <->
2024-12-01 21:20:46 +0530

    Added extra stylings to the portfolio page

--- src/Experience.js
+++ src/Experience.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./Project.css";
 import exp from "./Experience.json";
 const Experience = () => {
-  const companyExp = exp.map((item) => {
+  const companyExp = exp.map((item,index) => {
     return (
-      <div className="project-card">
+      <div className={(index==0)?"project-card type1":"project-card type2"}>
         <h3>{item.company}</h3>
         <h4>{item.position}</h4>
         <ul>
           {item.description.map((desc) => {
-            return <li>{desc}</li>;
+            return <li key={desc}>{desc}</li>;
           })}
         </ul>
       </div>
@@ -19,11 +19,7 @@ const Experience = () => {
     <section id="projects" className="container">
       <h2>Experience</h2>
       <div className="project-grid">
-        <div className="project-card">
-          {/* <h3>Project 1</h3>
-          <p>A brief description of Project 1.</p>*/}
-          {companyExp}
-        </div>
+        {companyExp}
       </div>
     </section>
   );
